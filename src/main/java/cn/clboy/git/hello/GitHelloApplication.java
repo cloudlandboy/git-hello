67921b9e278ad464a3e211da318750e97411886f
cloudlandboy <617751303@qq.com>
2021-05-31 11:42:48 +0800

    Initial commit

---
+++ src/main/java/cn/clboy/git/hello/GitHelloApplication.java
@@ -0,0 +1,13 @@
+package cn.clboy.git.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHelloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/clboy/git/hello/GitHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.clboy.git.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
